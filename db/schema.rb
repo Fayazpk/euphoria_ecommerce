b62eeca7511958b92e178c16b4f7c11c399c3c14
MOHAMMED FAYAZ ISMAIL P K <fayazpk18@gmail.com>
2025-02-04 13:38:52 +0530

    version changed

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_02_04_022142) do
+ActiveRecord::Schema[8.0].define(version: 2025_02_04_022142) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension "pg_catalog.plpgsql"
 
   create_table "posts", force: :cascade do |t|
     t.text "title"
@@ -20,5 +20,4 @@ ActiveRecord::Schema[7.1].define(version: 2025_02_04_022142) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
 end
